6189d75e6bb04ec8199a5d5e3e5cdff4452ad502
DanielGA <dga_355@hotmail.com>
2025-10-23 13:53:58 -0600

    Add postman E2E tests

--- app/go/src/entities/subscription.go
+++ app/go/src/entities/subscription.go
@@ -1,15 +1,15 @@
 package entities
 
 type SubscriptionItem struct {
-	PK             string                 `dynamodbav:"pk"`
-	SK             string                 `dynamodbav:"sk"`
-	Type           string                 `dynamodbav:"type"`
-	PlanSKU        string                 `dynamodbav:"planSku"`
-	StartDate      string                 `dynamodbav:"startDate"`
-	ExpiresAt      string                 `dynamodbav:"expiresAt"`
-	CancelledAt    *string                `dynamodbav:"cancelledAt"`
-	LastModifiedAt string                 `dynamodbav:"lastModifiedAt"`
-	Attributes     map[string]interface{} `dynamodbav:"attributes"`
+	PK             string         `dynamodbav:"pk"`
+	SK             string         `dynamodbav:"sk"`
+	Type           string         `dynamodbav:"type"`
+	PlanSKU        string         `dynamodbav:"planSku"`
+	StartDate      string         `dynamodbav:"startDate"`
+	ExpiresAt      string         `dynamodbav:"expiresAt"`
+	CancelledAt    *string        `dynamodbav:"cancelledAt"`
+	LastModifiedAt string         `dynamodbav:"lastModifiedAt"`
+	Attributes     map[string]any `dynamodbav:"attributes"`
 }
 
 type Plan struct {
